b367bbd96d0d00f0d9ab4932a52185e817463beb
Nikhil <53379695+nikhilvasaikar@users.noreply.github.com>
2021-06-30 16:32:35 +0200

    Update ProductRepository.java

--- src/main/java/com/gfgtech/product/repository/ProductRepository.java
+++ src/main/java/com/gfgtech/product/repository/ProductRepository.java
@@ -3,6 +3,4 @@ package com.gfgtech.product.repository;
 import com.gfgtech.product.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
-    void deleteById(Long id);
-}
+public interface ProductRepository extends JpaRepository<Product, Long> {}
